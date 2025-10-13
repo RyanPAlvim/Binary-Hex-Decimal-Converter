f1b91ef5ea1f0901399467d364568f47c6f3f9e8
RyanPAalvim <ryalvim84@gmail.com>
2025-10-13 01:32:15 -0300

    Add function to convert binary to decimal

--- src/br/com/numcoverter/numsys/Binary.java
+++ src/br/com/numcoverter/numsys/Binary.java
@@ -1,8 +1,18 @@
 package br.com.numcoverter.numsys;
 
 public class Binary {
-    public static int toDecimal(String binaryNum){
-        
-        return 2;
+    public static double toDecimal(String binaryNum){
+
+        double sum = 0;
+        int index = 0;
+
+        for(int i = binaryNum.length() - 1; i >= 0; i--){
+            if(binaryNum.charAt(i) == '1'){
+                sum += Math.pow(2, index);
+            }
+            index++;
+        }
+
+        return sum;
     }
 }
